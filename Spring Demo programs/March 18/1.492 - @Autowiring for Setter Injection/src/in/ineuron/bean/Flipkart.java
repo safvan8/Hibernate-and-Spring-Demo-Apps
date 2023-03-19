574c8958afa4acf724463924958e9154f8a3a303
Safvan <safvanpmcz@gmail.com>
2023-03-19 08:43:13 +0530

    Auto wiring using setter injection program added

---
+++ Spring Demo programs/March 18/1.492 - @Autowiring for Setter Injection/src/in/ineuron/bean/Flipkart.java
@@ -0,0 +1,60 @@
+package in.ineuron.bean;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+//Target Object
+public class Flipkart
+{
+
+	// Dependent Object
+	// Default autowiring is "byType"
+	private Courier courier;
+
+	// setter for performing Setter Injection
+	@Autowired
+	public void setCourier(@Qualifier("fFlight") Courier courier)
+	{
+		this.courier = courier;
+		System.out.println("Flipkart.setCourier(courier) ----Setter injection");
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Flipkart [courier=" + courier + "]";
+	}
+
+	static
+	{
+		System.out.println("Flipkart.class file is loading...");
+	}
+
+	public Flipkart()
+	{
+		System.out.println("Flipkart object is created...");
+	}
+
+	// BUISNESS LOGIC
+	public String shopping(String[] items, float[] prices)
+	{
+		System.out.println("Flipkart.shopping()");
+		System.out.println(courier.getClass().getName());
+		Random random = null;
+		float billAmt = 0.0f;
+		int oid = 0;
+		for (float price : prices)
+		{
+			billAmt += price;
+		}
+		random = new Random();
+		oid = random.nextInt(1000);
+		String msg = courier.deliver(oid);
+
+		return Arrays.toString(items) + "are purchase having prices " + Arrays.toString(prices)
+				+ " with the bill amount :: " + billAmt + "---> " + msg;
+	}
+}
